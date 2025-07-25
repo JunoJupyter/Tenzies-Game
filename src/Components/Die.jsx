d46674a41f617f9c99266042cc1506e20bafed4c
Ayush <ayushmultiverse@gmail.com>
2025-07-25 22:10:50 +0530

    All functionality added

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
-
 function Die(props) {
-  const [fixed, setFixed] = useState(false);
-  const [finalValue, setFinalValue] = useState(null);
-  const [isRolling, setIsRolling] = useState(false);
-
-  function fixDiceValue() {
-    setFinalValue(props.value);
-    setFixed((prev) => !prev);
-  }
-
-  // Detect change in props.value to trigger animation
-  useEffect(() => {
-    if (!fixed) {
-      setIsRolling(true);
-      const timer = setTimeout(() => {
-        setIsRolling(false);
-      }, 300);
-      return () => clearTimeout(timer);
-    }
-  }, [props.rollTrigger]);
-
   return (
     <button
-      type="submit"
-      onClick={fixDiceValue}
-      className={`dice ${fixed ? "fixedDice" : ""} ${
-        isRolling ? "rolling" : ""
-      }`}
+      type="button"
+      onClick={() => props.freeze(props.id)}
+      className={props.isFixed ? "dice fixedDice" : "dice"}
     >
-      {fixed ? finalValue : props.value}
+      {props.value}
     </button>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,33 +1,88 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Die from "./Die";
+import ShortUniqueId from "short-unique-id";
+import Confetti from "react-confetti";
+const uid = new ShortUniqueId({ length: 5 });
 
 function Main() {
-  const [arr, setArr] = useState(generateRandomNumbers());
+  // const [arr, setArr] = useState(generateRandomNumbers());
   const [rollCount, setRollCount] = useState(0);
+  const [dieInfo, setDieInfo] = useState(() => generateRandomNumbers());
   // generateRandomNumbers();
 
   function generateRandomNumbers() {
     const temp = [];
     for (let i = 0; i <= 9; i++) {
-      temp.push(Math.ceil(Math.random() * 9));
+      const currObj = {
+        id: uid.rnd(),
+        value: Math.ceil(Math.random() * 6),
+        isFixed: false,
+      };
+      temp.push(currObj);
     }
+    // console.log(temp);
     return temp;
   }
 
-  function rollDie() {
-    setArr(generateRandomNumbers());
+  const checkWin =
+    dieInfo.every((dice) => dice.isFixed) &&
+    dieInfo.every((dice) => dice.value === dieInfo[0].value);
+
+  // if (checkWin) {
+  //   console.log("Won!! \n New Game ?");
+  // }
+
+  function rollDie(event) {
+    console.log(event.target.innerText);
+    const currText = event.target.innerText;
+    if (currText === "New Game!!") {
+      setDieInfo(generateRandomNumbers());
+      return;
+    }
+    setDieInfo((dice) => {
+      return dice.map((dice) => {
+        return dice.isFixed
+          ? { ...dice }
+          : {
+              ...dice,
+              value: Math.ceil(Math.random() * 6),
+            };
+      });
+    });
+
     setRollCount((prev) => prev + 1);
   }
 
-  const dieElement = arr.map((value, index) => (
-    <Die key={index} value={value} rollTrigger={rollCount} />
+  function freeze(id) {
+    setDieInfo((prev) =>
+      prev.map((elem) =>
+        elem.id === id ? { ...elem, isFixed: !elem.isFixed } : elem
+      )
+    );
+  }
+
+  // function startNewGame() {
+  //   console.log("new game started");
+  //   setDieInfo(generateRandomNumbers());
+  // }
+
+  const dieElement = dieInfo.map((diceInfo) => (
+    <Die
+      key={diceInfo.id}
+      id={diceInfo.id}
+      value={diceInfo.value}
+      isFixed={diceInfo.isFixed}
+      freeze={freeze}
+      rollTrigger={rollCount}
+    />
   ));
 
   return (
     <main className="gamesection">
+      {checkWin ? <Confetti /> : null}
       <div className="die-container">{dieElement}</div>
       <button type="button" className="rolldie-btn" onClick={rollDie}>
-        Roll
+        {checkWin ? "New Game!!" : "Roll"}
       </button>
     </main>
   );
